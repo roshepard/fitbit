29b3e9c0d441994fecf372b6e0534216a9a9dc12
Tom Hummel <tphummel@gmail.com>
2016-03-23 22:54:20 -0700

    fixes for authorization_code script

--- get_fitbit_stepped.sh
+++ get_fitbit_stepped.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/usr/bin/env bash
 set -x
 
 personal_key=`echo -n '123ABC:slkdjfslkdfjslkdjfslkdjfslkdfc92' | openssl base64`
@@ -10,6 +10,10 @@ curl \
   -i -v -X POST \
   --header "Content-Type: application/x-www-form-urlencoded" \
   --header "Authorization: Basic $personal_key" \
-  https://api.fitbit.com/oauth2/token?client_id=123ABC&grant_type=authorization_code&redirect_uri=http://www.ahwair.com&code=9a2bd3f5f9bcae9c44ca645ea804a0ac74e2fafc
+  --data-urlencode "client_id=123ABC" \
+  --data-urlencode "grant_type=authorization_code" \
+  --data-urlencode "redirect_uri=http://www.ahwair.com" \
+  --data-urlencode "code=9a2bd3f5f9bcae9c44ca645ea804a0ac74e2fafc" \
+  https://api.fitbit.com/oauth2/token
 
 set +x
